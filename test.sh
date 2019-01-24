8aac1c28bd04277d06a4c234d0be0186c4f95ede
Guilherme Oliveira <guilhermeoliveira564@gmail.com>
2019-01-24 14:04:49 -0200

    Add Swagger Docs

--- test.sh
+++ test.sh
@@ -1,18 +1,29 @@
-curl -H "Content-Type: application/json" -X POST  http://localhost:8080/games/ | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "9" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "1" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "4" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "6" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "2" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "1" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "3" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "1" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls | jq .
-curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls | jq .
-curl http://localhost:8080/scores/2/ | jq .
+curl -H "Content-Type: application/json" -X POST  http://localhost:8080/games/ 
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "9" }' http://localhost:8080/games/2/rolls
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "1" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "4" }' http://localhost:8080/games/2/rolls 
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "6" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "2" }' http://localhost:8080/games/2/rolls 
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "1" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "3" }' http://localhost:8080/games/2/rolls 
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "1" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls
+
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls
+curl -H "Content-Type: application/json"  -X POST -d '{ "pinsHit": "10" }' http://localhost:8080/games/2/rolls
+
+curl http://localhost:8080/scores/2/
