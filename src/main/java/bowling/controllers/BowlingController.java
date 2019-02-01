9a573296d0939e5df26fa49819397414d1ab7aef
Guilherme Oliveira <guilhermeoliveira564@gmail.com>
2019-02-01 17:08:49 -0200

    Enable CORS

--- src/main/java/bowling/controllers/BowlingController.java
+++ src/main/java/bowling/controllers/BowlingController.java
@@ -5,6 +5,7 @@ import bowling.models.Roll;
 import bowling.models.RollRequest;
 import bowling.services.BowlingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
